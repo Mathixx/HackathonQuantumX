ace3641b1bcdd68eff549208c984356ed904ceed
Moncef <moncef.djafri@polytechnique.edu>
2024-11-24 14:17:34 +0100

    agent in use

--- Manager/tool_config.py
+++ Manager/tool_config.py
@@ -5,13 +5,13 @@ tools_conf = [
         "type": "function",
         "function": {
             "name": "get_k_nearests_product",
-            "description": "Search for the k products closest to the user's query.",
+            "description": "Search for the k products closest to the user's needs. This function is to be used when looking for products to suggest to the client. If this information is already available, it should not be called.",
             "parameters": {
                 "type": "object",
                 "properties": {
                     "query": {
                         "type": "string",
-                        "description": "The query string to search for similar products."
+                        "description": "Description of the products to look for."
                     },
                     "k": {
                         "type": "integer",
@@ -27,103 +27,8 @@ tools_conf = [
     {
         "type": "function",
         "function": {
-            "name": "get_k_nearest_users",
-            "description": "Search for users who have a similar profile to the target user.",
-            "parameters": {
-                "type": "object",
-                "properties": {
-                    "user_id": {
-                        "type": "integer",
-                        "description": "The unique identifier for the user."
-                    },
-                    "k": {
-                        "type": "integer",
-                        "description": "The number of nearest users to retrieve.",
-                        "default": 3
-                    }
-                },
-                "required": ["user_id"],
-                "additionalProperties": False
-            }
-        }
-    },
-    {
-        "type": "function",
-        "function": {
-            "name": "get_best_purchases_from_neighbours",
-            "description": "Retrieve the best products purchased by users with similar profiles.",
-            "parameters": {
-                "type": "object",
-                "properties": {
-                    "user_id": {
-                        "type": "integer",
-                        "description": "The unique identifier for the user.",
-                        "default": 0
-                    }
-                },
-                "required": ["user_id"],
-                "additionalProperties": False
-            }
-        }
-    },
-    {
-        "type": "function",
-        "function": {
-            "name": "get_not_delivered",
-            "description": "Returns a text summarizing the undelivered purchases of the user.",
-            "parameters": {
-                "type": "object",
-                "properties": {
-                    "user_id": {
-                        "type": "integer",
-                        "description": "The unique identifier for the user."
-                    }
-                },
-                "required": ["user_id"],
-                "additionalProperties": False
-            }
-        }
-    },
-    {
-        "type": "function",
-        "function": {
-            "name": "handle_insufficient_info",
-            "description": "Handles cases where there is not enough information to proceed with a product search and requests additional details from the user.",
-            "parameters": {
-                "type": "object",
-                "properties": {
-                    "missing_fields": {
-                        "type": "array",
-                        "items": {
-                            "type": "string",
-                            "description": "The list of fields that are missing or insufficient for the search."
-                        },
-                        "description": "The specific fields that are missing from the input, requiring user input."
-                    },
-                    "suggested_fields": {
-                        "type": "array",
-                        "items": {
-                            "type": "string",
-                            "description": "The list of fields that the system suggests the user to provide next."
-                        },
-                        "description": "Fields that are most critical to gather additional information for a search."
-                    },
-                    "action": {
-                        "type": "string",
-                        "enum": ["request_info", "abort"],
-                        "description": "Action to be taken: request more information or abort the process."
-                    }
-                },
-                "required": ["missing_fields", "action"],
-                "additionalProperties": False
-            }
-        }
-    },
-    {
-        "type": "function",
-        "function": {
-            "name": "check_user_purchase_history",
-            "description": "Analyzes user purchase history and current orders to identify relevant patterns or preferences for product recommendations.",
+            "name": "user_purchase_history",
+            "description": "Retrieves the list of purchases made by the user. This should be called whenever the user asks for his previous purchases, or we need to look at his  purchases to recommend new products. Else , it should not be called.",
             "parameters": {
                 "type": "object",
                 "properties": {
@@ -140,5 +45,15 @@ tools_conf = [
                 "additionalProperties": False
             }
         }
+    },
+    {
+        "type": "function",
+        "function": {
+            "name": "no_more_info_needed",
+            "description": "Indicates that no more information is needed for the current task. This should be called whenever the query is empty",
+            "parameters": {}
+        }
     }
 ]
+
+
